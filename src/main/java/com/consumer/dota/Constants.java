d655ce018a0e4c12744bf1ce18975e03ffc0dc6b
Felipe Cardozo <felipe.cardozo@gmail.com>
2020-08-09 12:01:48 -0500

    updates spring version to 2.3.1.RELEASE, refactors mongoclient to auto configured by springboot

--- src/main/java/com/consumer/dota/Constants.java
+++ src/main/java/com/consumer/dota/Constants.java
@@ -2,9 +2,9 @@ package com.consumer.dota;
 
 public class Constants {
 	
-	public final static String DB_NAME = "dota2";
-	public final static String DB_HOST = "127.0.0.1";
-	public final static Integer DB_PORT = 27017;
+	//public final static String DB_NAME = "dota2";
+	//public final static String DB_HOST = "127.0.0.1";
+	//public final static Integer DB_PORT = 27017;
 	
 	public static final String URL = "https://api.opendota.com/api";
 	
